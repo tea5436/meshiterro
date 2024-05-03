1fa94815e539e079c34879beb2dfdf6cb276f295
tea5436 <158519375+tea5436@users.noreply.github.com>
2024-05-03 00:49:45 +0000

    [Start] 4-10

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get 'post_images/new'
-  get 'post_images/index'
-  get 'post_images/show'
   devise_for :users
   get 'homes/top'
   root to: "homes#top"
+
+  resources :post_images, only: [:new, :index, :show]
+
   get 'homes/about',to: 'homes#about',as: 'about'
 end
 
- 
