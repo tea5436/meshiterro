4cee0b708f87d1163565325ddd19a79b35fa4dda
tea5436 <158519375+tea5436@users.noreply.github.com>
2024-05-06 01:18:04 +0000

    [Finish] 4-18

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-         has_many :post_images, dependent: :destroy
-         
-         
+  devise:database_authenticatable, :registerable,
+        :recoverable, :rememberable, :validatable
+        
+  has_many :post_images, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
   has_one_attached :profile_image
   
   def get_profile_image(width, height)
